407a38b6e3285aa5a030ae768bcbd843ee7f7026
umwalimaureen11-stack <umwalimaureen11@gmail.com>
2026-03-19 11:19:17 +0200

    commit4

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleNavbar() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
